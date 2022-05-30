43e3ce90fbfc2b235ed4ca9d261d3fb78b0fb6d0
Kamalikajash <kamalikapriya@gmail.com>
2022-05-30 09:14:19 +0530

    user chat consumer added

--- ChatApi/consumers.py
+++ ChatApi/consumers.py
@@ -6,17 +6,10 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.user = self.scope["user"]
         self.room_group_name = 'chat_%s' % self.room_name
-        self.user_group_name = 'user_%d' % self.user.id
 
         # Join room group
         await self.channel_layer.group_add(
             self.room_group_name,
-            self.channel_name
-        )
-
-        # Join user group
-        await self.channel_layer.group_add(
-            self.user_group_name,
             self.channel_name
         )
 
@@ -37,6 +30,48 @@ class ChatConsumer(AsyncWebsocketConsumer):
         # Send message to room group
         await self.channel_layer.group_send(
             self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': text_data
+            }
+        )
+
+    # Receive message from room group
+    async def chat_message(self, event):
+        message = event['message']
+
+        # Send message to WebSocket
+        await self.send(text_data=message)
+
+class UserChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.user_id = self.scope['url_route']['kwargs']['user_id']
+        self.user = self.scope["user"]
+        self.user_group_name = 'user_%d' % self.user.id
+
+        # Join user group
+        await self.channel_layer.group_add(
+            self.user_group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        # Leave room group
+        await self.channel_layer.group_discard(
+            self.user_group_name,
+            self.channel_name
+        )
+
+    # Receive message from WebSocket
+    async def receive(self, text_data):
+        # text_data_json = json.loads(text_data)
+        # message = text_data_json['message']
+
+        # Send message to room group
+        await self.channel_layer.group_send(
+            self.user_group_name,
             {
                 'type': 'chat_message',
                 'message': text_data
--- ChatApi/routing.py
+++ ChatApi/routing.py
@@ -4,4 +4,5 @@ from ChatApi import consumers
 
 websocket_urlpatterns = [
     re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
+    re_path(r'ws/chat/(?P<user_id>\w+)/$', consumers.UserChatConsumer.as_asgi()),
 ]
